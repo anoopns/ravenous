6e145edca2e70fa122e5e59702cdfc71622726eb
anoopns <ns.anoop@gmail.com>
2021-11-04 11:02:49 +1100

    finishing app with static data

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -31,7 +31,7 @@ class Business extends React.Component {
               </div>
               <div className="Business-reviews">
                 <h3>{business.category.toUpperCase()}</h3>
-                <h3 className="rating">(business.rating)</h3>
+                <h3 className="rating">{business.rating}</h3>
                 <p>{business.reviewCount} reviews</p>
               </div>
             </div>
